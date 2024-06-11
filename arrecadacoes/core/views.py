a3ecf9d60f7f76980e263a5304f503db650a7d23
MateUZZOO7 <103078875+MateUZZOO7@users.noreply.github.com>
2024-06-11 15:40:08 -0300

    Add files via upload
    Atualização v7

--- arrecadacoes/core/views.py
+++ arrecadacoes/core/views.py
@@ -1,13 +1,15 @@
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
 from django.urls import reverse
 from django.core.exceptions import ValidationError
 from .forms import CadastroForm
-from .services import CadastroClienteService  # Importação correta do serviço
+from .services import CadastroClienteService
 from django.contrib.auth.decorators import login_required
 from .models import Doacao
 from django.contrib import messages
+
 
 def index(request):
     return render(request, 'index.html')
@@ -39,20 +41,27 @@ def cadastro_view(request):
 
 def login_view(request):
     if request.method == 'POST':
+        print("Formulário de login submetido!")
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
+            print("Dados do formulário são válidos!")
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             user = authenticate(request, username=username, password=password)
             if user is not None:
+                print("Usuário autenticado com sucesso!")
                 login(request, user)
-                return redirect(request.GET.get('next', reverse('core:index')))  # Redireciona para o perfil do usuário
+                return redirect(reverse('core:index'))  # Redireciona para a página inicial após o login
             else:
-                form.add_error(None, 'Usuário ou senha inválidos.')
+                print("Usuário não autenticado. Credenciais inválidas.")
+                # Lida com credenciais inválidas
+                return render(request, 'login.html', {'form': form, 'error_message': 'Credenciais inválidas.'})
     else:
         form = AuthenticationForm()
 
     return render(request, 'login.html', {'form': form})
+
+
 
 def ongs_view(request):
     return render(request, 'ongs.html')
@@ -64,10 +73,12 @@ def fazer_doacao(request):
         item_doado = request.POST.get('item_doado')
         quantidade_doada = request.POST.get('quantidade_doada')
 
-        donate = request.user
+        # Obtém o usuário autenticado
+        user = request.user  
 
+        # Cria a doação associada ao usuário autenticado
         doacao = Doacao.objects.create(
-            donante=donate,
+            user=user,  # Associa a doação ao usuário autenticado
             ong_nome=ong_nome,
             item_doado=item_doado,
             quantidade_doada=quantidade_doada
